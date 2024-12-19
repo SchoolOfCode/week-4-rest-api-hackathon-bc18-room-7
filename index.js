92265c1536ad721ae2f48ee3dce699a9f83dc43a
Jacob Smith <183931179+SmithyJ98@users.noreply.github.com>
2024-12-19 14:28:44 +0000

    Made a start on post request

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-import { getPlayers } from "./players.js";
+import { getPlayerByShirtNumber, getPlayers, createPlayer } from "./players.js";
 
 app.use(express.json());
 
+//GET request for all players to show
 app.get("/england", async function (req, res) {
   const players = await getPlayers();
   res.json(players);
+});
+
+//GET request for specific player to show by using their shirt number
+app.get("/england/:shirtNumber", async function (req, res) {
+  const shirtNumber = req.params.shirtNumber;
+  const players = await getPlayerByShirtNumber(shirtNumber);
+  res.json(players);
+});
+
+app.post("/england", async (req, res) => {
+  const newPlayer = await createPlayer(req.body);
+  res.json(newPlayer);
 });
 
 app.listen(PORT, function () {
--- players.js
+++ players.js
@@ -4,3 +4,20 @@ export async function getPlayers() {
   const players = await readPlayers();
   return players;
 }
+
+export async function getPlayerByShirtNumber(shirtNumber) {
+  const players = await readPlayers();
+
+  const found = players.find((england) => england.shirtNumber === shirtNumber);
+
+  return found;
+}
+
+export async function createPlayer(newPlayer) {
+  const created = {
+    ...newPlayer,
+  };
+
+  england = [...england, created];
+  return created;
+}
